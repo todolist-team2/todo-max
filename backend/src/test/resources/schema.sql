aa9b8003f4ccc3598f774006cb04dc690ef70d4a
Jeongyong Park <1239824@naver.com>
2023-07-10 17:52:29 +0900

    chore: DB 스키마 작성

---
+++ backend/src/test/resources/schema.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS card;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS user_account;
+DROP TABLE IF EXISTS action;
+
+CREATE TABLE card
+(
+    id            BIGINT       NOT NULL AUTO_INCREMENT,
+    title         VARCHAR(64)  NOT NULL,
+    content       VARCHAR(255) NOT NULL,
+    created_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    modified_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
+    category_id   BIGINT       NOT NULL,
+    child_card_id BIGINT       NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE category
+(
+    id              BIGINT      NOT NULL AUTO_INCREMENT,
+    name            VARCHAR(64) NOT NULL,
+    user_account_id BIGINT      NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE user_account
+(
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
+    login_id   VARCHAR(64)  NOT NULL,
+    password   VARCHAR(512) NOT NULL,
+    nickname   VARCHAR(64)  NOT NULL,
+    image_url  VARCHAR(512) NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE action
+(
+    id                   BIGINT      NOT NULL AUTO_INCREMENT,
+    action_name          VARCHAR(64) NOT NULL,
+    card_name            VARCHAR(64) NOT NULL,
+    origin_category_name VARCHAR(64) NOT NULL,
+    target_category_name VARCHAR(64) NOT NULL,
+    user_id              BIGINT      NOT NULL,
+    PRIMARY KEY (`id`)
+);
