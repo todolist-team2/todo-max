bfa8e8c6b599ddb7c6d6115924c1158e9ca14461
Jeongyong Park <1239824@naver.com>
2023-07-12 12:15:18 +0900

    fix: `LIMIT 1`을 제거하도록 쿼리 수정
    `EXISTS` 쿼리가 `LIMIT 1`가 동일한 성능을 내기 때문에 제거

--- backend/src/main/java/kr/codesquad/todo/repository/CategoryRepository.java
+++ backend/src/main/java/kr/codesquad/todo/repository/CategoryRepository.java
@@ -1,9 +1,9 @@
 package kr.codesquad.todo.repository;
 
-import java.util.Map;
-
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 public class CategoryRepository {
@@ -15,7 +15,7 @@ public class CategoryRepository {
 	}
 
 	public Boolean existById(Long categoryId) {
-		String existById = "SELECT EXISTS (SELECT id FROM category WHERE id = :id) LIMIT 1";
+		String existById = "SELECT EXISTS (SELECT id FROM category WHERE id = :id)";
 		return jdbcTemplate.queryForObject(existById, Map.of("id", categoryId), Boolean.class);
 	}
 }
