ef61dca9f4ef4d0a692cadfcff643be838c0a3b9
hjsong <song6240@naver.com>
2023-07-17 13:35:20 +0900

    chore: msw 세팅

---
+++ frontend/src/mocks/handlers.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable no-undef */
+import { rest } from "msw";
+
+const todos = ["먹기", "자기", "달리기"];
+
+export const handlers = [
+  rest.get("/todos", async (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(todos));
+  }),
+  // 전체 카드 목록 불러오기
+  rest.get('/api/cards', async(req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(allCards));
+  })
+
+  // 카드 추가
+
+  // 카드 삭제
+
+  // 카드 수정
+
+  // 카드 이동
+];
+
+const allCards = [
+  {
+    "categoryId": 1,
+    "categoryName": "todo",
+    "cards": [
+      {
+        "id": 1,
+        "title": "title1",
+        "content": "content",
+        "nickname": "nickname"
+      },
+      {
+        "id": 2,
+        "title": "title2",
+        "content": "content2",
+        "nickname": "nickname2"
+      }
+    ]
+  },
+  {
+    "categoryId": 2,
+    "categoryName": "doing",
+    "cards": [
+      {
+        "id": 1,
+        "title": "title1",
+        "content": "content",
+        "nickname": "nickname"
+      },
+      {
+        "id": 2,
+        "title": "title2",
+        "content": "content2",
+        "nickname": "nickname2"
+      }
+    ]
+  }
+]
