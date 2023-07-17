ecee8116fa14304284aef8823a41d49172f2b52f
hjsong <song6240@naver.com>
2023-07-17 15:28:33 +0900

    feat: 카드 등록 버튼 활성화
    - 제목, 내용을 모두 입력하면 등록 버튼 활성화함.

--- frontend/src/components/main/board/column/card/CardForm.tsx
+++ frontend/src/components/main/board/column/card/CardForm.tsx
@@ -1,5 +1,5 @@
-import { styled } from "styled-components";
 import { useState } from "react";
+import { styled } from "styled-components";
 import TTheme from "../../../../../types/TTheme";
 
 function CardForm({
@@ -16,17 +16,31 @@ function CardForm({
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
+  const isSubmitBtnClickable = title.length !== 0 && content.length !== 0;
+
   return (
     <StyledCard variant={variant}>
       <h4 className="blind">카드</h4>
       <div className="inner">
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
-        <textarea name="content" id="" maxLength={500} value={content} onChange={(e) => setContent(e.target.value)}></textarea>
-        <div>
-          <button onClick={handleCancelButtonClick}>취소</button>
-          <button>{mode === "add" ? "등록" : "저장"}</button>
-        </div>
+        <input type="text" value={title} placeholder="제목을 입력하세요" onChange={(e) => setTitle(e.target.value)} />
+        <textarea
+          name="content"
+          id=""
+          rows={1}
+          maxLength={500}
+          value={content}
+          placeholder="내용을 입력하세요"
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
       </div>
+      <ul className="buttons">
+        <li>
+          <button onClick={handleCancelButtonClick}>취소</button>
+        </li>
+        <li>
+          <button disabled={!isSubmitBtnClickable}>{mode === "add" ? "등록" : "저장"}</button>
+        </li>
+      </ul>
     </StyledCard>
   );
 }
@@ -38,28 +52,44 @@ const StyledCard = styled.article<{ theme: TTheme; variant: "default" | "drag" |
   background-color: ${(props) => props.theme.color.surface.default};
   box-shadow: ${(props) => (props.variant === "drag" ? props.theme.boxShadow.floating : props.theme.boxShadow.normal)};
   display: flex;
-  align-items: flex-start;
+  flex-direction: column;
+  justify-content: flex-start;
   gap: 16px;
   opacity: ${(props) => (props.variant === "place" ? 0.3 : 1)};
   margin-bottom: 10px;
 
   .inner {
-    flex-grow: 1;
+    width: 240px;
+  }
+
+  input {
+    width: 100%;
+    margin-bottom: 8px;
+    border: none;
+    font: ${(props) => props.theme.font.display.bold14};
+    color: ${(props) => props.theme.color.text.strong};
+
+    &:focus {
+      outline: 2px solid ${(props) => props.theme.color.grayscale[300]};
+    }
   }
 
   textarea {
+    width: 100%;
     resize: none;
+    border: none;
+    font: ${(props) => props.theme.font.display.medium14};
+    color: ${(props) => props.theme.color.text.default};
+
+    &:focus {
+      outline: 2px solid ${(props) => props.theme.color.grayscale[300]};
+    }
   }
 
-  ul {
-    flex-shrink: 0;
-    button {
-      display: block;
-      background-color: transparent;
-      border: 0;
-      padding: 0;
-      cursor: pointer;
-    }
+  .buttons {
+    width: 100%;
+    display: flex;
+    gap: 8px;
   }
 `;
 
