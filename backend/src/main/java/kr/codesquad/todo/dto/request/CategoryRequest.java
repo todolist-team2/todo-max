75ef7568ef90f0b13598bcac2ae31b7452b74925
HyowonSin <tlsgydnjs@naver.com>
2023-07-17 16:56:48 +0900

    refactor: jdbc 로직 간소화

--- backend/src/main/java/kr/codesquad/todo/dto/request/CategoryRequest.java
+++ backend/src/main/java/kr/codesquad/todo/dto/request/CategoryRequest.java
@@ -8,6 +8,9 @@ public class CategoryRequest {
 	@NotBlank(message = "카테고리 이름을 입력해주세요.")
 	private String name;
 
+	public CategoryRequest() {
+	}
+
 	public CategoryRequest(String name) {
 		this.name = name;
 	}
