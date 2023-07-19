442dd3e2e3c26ffb50f60ea9503ea4212264e807
hjsong <song6240@naver.com>
2023-07-19 22:18:30 +0900

    feat: 카드 드래그 시 카드가 마우스를 따라 이동

--- frontend/src/components/main/board/column/card/Card.tsx
+++ frontend/src/components/main/board/column/card/Card.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState, useCallback } from "react";
 import { css, styled } from "styled-components";
 import { useAlert } from "../../../../../hooks/useAlert";
 import TTheme from "../../../../../types/TTheme";
@@ -7,16 +7,69 @@ import Buttons from "../../../../common/Buttons";
 type TMode = "Default" | "Add/Edit" | "Drag" | "Place";
 
 const Card = styled(
-  ({ className, title, content, onDelete, onEdit }: { className?: string; title: string; content: string; onDelete: () => Promise<void>, onEdit: () => void }) => {
+  ({
+    className,
+    title,
+    content,
+    onDelete,
+    onEdit,
+  }: {
+    className?: string;
+    title: string;
+    content: string;
+    onDelete: () => Promise<void>;
+    onEdit: () => void;
+  }) => {
     const [mode, setMode] = useState<TMode>("Default");
+    const [isMouseDown, setIsMouseDown] = useState(false);
+    const [dragOffset, setDragOffset] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+    const [position, setPosition] = useState<{ x: number; y: number }>();
 
     function editFormSubmitHandler(event: FormEvent<HTMLFormElement>): void {
       event.preventDefault();
       console.log(event.target);
     }
 
+    const handleMouseDown = (e: React.MouseEvent) => {
+      const cardRect = e.currentTarget.getBoundingClientRect();
+      const offsetX = e.clientX - cardRect.left;
+      const offsetY = e.clientY - cardRect.top;
+      setDragOffset({ x: offsetX, y: offsetY });
+      setIsMouseDown(true);
+    };
+
+    const handleMouseMove = useCallback((e: MouseEvent) => {
+      setPosition({
+        x: e.clientX - dragOffset.x,
+        y: e.clientY - dragOffset.y,
+      });
+    }, [dragOffset]);
+
+    const handleMouseUp = () => {
+      setIsMouseDown(false);
+    };
+
+    const positionStyle: React.CSSProperties = position
+      ? {
+          position: "absolute",
+          left: `${position.x}px`,
+          top: `${position.y}px`,
+        }
+      : {};
+
+    useEffect(() => {
+      if (!isMouseDown) {
+        return;
+      }
+      window.addEventListener("mousemove", handleMouseMove);
+
+      return () => {
+        window.removeEventListener("mousemove", handleMouseMove);
+      };
+    }, [isMouseDown, handleMouseMove]);
+
     return (
-      <article className={className} data-mode={mode}>
+      <article className={className} data-mode={mode} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} style={positionStyle}>
         <h4 className="blind">카드</h4>
         {mode !== "Add/Edit" && (
           <div className="container">
@@ -75,6 +128,7 @@ const Card = styled(
   ${({ theme }) => {
     const { color, font } = theme;
     return css`
+      width: 300px;
       padding: 16px;
       border-radius: 8px;
       background-color: ${color.surface.default};
